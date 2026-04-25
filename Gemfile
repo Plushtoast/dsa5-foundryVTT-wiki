53d094477bb5876b3ad12530bd755b521621bda9
Jan <janweigmann@web.de>
2026-04-25 08:09:25 +0200

    Publish Pages site from source repo

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll', '~> 4.4.1'
-gem 'webrick', '~> 1.9'
--- vendor/bundle/ruby/3.2.0/gems/ruby-lsp-0.26.9/lib/ruby-lsp.rb
+++
@@ -1,6 +0,0 @@
-# typed: true
-# frozen_string_literal: true
-
-module RubyLsp
-  VERSION = File.read(File.expand_path("../VERSION", __dir__)).strip
-end
